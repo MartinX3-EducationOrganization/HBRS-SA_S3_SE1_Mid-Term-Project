776f6d7507048f07e4737c29da0afad7e7952f72
terenceskill <reinholdjourdan@hotmail.com>
2018-01-08 14:44:46 +0100

    Testing System.

--- src/org/bonn/se/ws17/midterm/Main.java
+++ src/org/bonn/se/ws17/midterm/Main.java
@@ -2,6 +2,6 @@ package org.bonn.se.ws17.midterm;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        System.out.println("¯\\_(ツ)_/¯");
     }
 }
