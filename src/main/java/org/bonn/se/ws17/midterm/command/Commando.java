a5fa71b669e0237b85336673331f342cb0a6e34a
terenceskill <reinholdjourdan@hotmail.com>
2018-01-23 18:59:06 +0100

    #13 update commando

---
+++ src/main/java/org/bonn/se/ws17/midterm/command/Commando.java
@@ -0,0 +1,11 @@
+package org.bonn.se.ws17.midterm.command;
+
+public class Commando {
+    
+    private String[] content;
+    
+    public String[] getContent() { return content; }
+    
+    public void setContent(String[] content) { this.content = content; }
+    
+}
