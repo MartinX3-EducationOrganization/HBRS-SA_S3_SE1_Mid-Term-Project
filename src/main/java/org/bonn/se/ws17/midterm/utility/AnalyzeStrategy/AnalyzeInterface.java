a5249821c68bc7c9c736db258922b7f01e19f524
Terenceskill <reinholdjourdan@hotmail.com>
2018-01-26 13:51:22 +0100

    close #26 set in main todo

---
+++ src/main/java/org/bonn/se/ws17/midterm/utility/AnalyzeStrategy/AnalyzeInterface.java
@@ -0,0 +1,5 @@
+package org.bonn.se.ws17.midterm.utility.AnalyzeStrategy;
+
+public interface AnalyzeInterface {
+    void analyze(String[] params);
+}
---
+++ src/main/java/org/bonn/se/ws17/midterm/utility/AnalyzeStrategy/Strategy.java
@@ -0,0 +1,22 @@
+package org.bonn.se.ws17.midterm.utility.AnalyzeStrategy;
+
+
+public class Strategy {
+    private AnalyzeInterface strategy = null;
+    private String[] params;
+    
+    public void setStrategy(AnalyzeInterface STRATEGIE) {
+        strategy = STRATEGIE;
+    }
+    
+    public void setParams(String[] params) {
+        this.params = params;
+    }
+    
+    public void analyze(String[] params) {
+        if (strategy != null) {
+            strategy.analyze(params);
+        }
+    }
+}
+
